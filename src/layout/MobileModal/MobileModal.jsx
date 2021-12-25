b8a1782c15f8d1c1a0a7281021a7f761c19b9435
jaimegsandoval <jaime.devs@gmail.com>
2021-12-24 16:08:57 -0800

    Add feedback bar and sorting option components
    Made a component for feedback bar where user can sort the feedback
    requests. Made a sorting_data object to pack all necessary data for the
    sorting option component

--- src/layout/MobileModal/MobileModal.jsx
+++ src/layout/MobileModal/MobileModal.jsx
@@ -4,6 +4,14 @@ import { Button } from './Button';
 import RoadMapData from './RoadMapData';
 import styles from './_mobileModal.module.scss';
 
+const BUTTON_TITLES = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature'];
+
+const roadMapData = [
+  { category: 'planned', tasks: 2 },
+  { category: 'in-progress', tasks: 6 },
+  { category: 'live', tasks: 4 },
+];
+
 export const MobileModal = ({ isModalOpen }) => {
   const [active, setActive] = useState(0);
 
@@ -12,14 +20,6 @@ export const MobileModal = ({ isModalOpen }) => {
   const handleClick = (index) => {
     setActive(index);
   };
-
-  const BUTTON_TITLES = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature'];
-
-  const roadMapData = [
-    { category: 'planned', tasks: 2 },
-    { category: 'in-progress', tasks: 6 },
-    { category: 'live', tasks: 4 },
-  ];
 
   return createPortal(
     <div className={styles.modalContainer}>
