f27bdce7aeacd194ab541b60cc3a3159342ebf60
jaimegsandoval <jaime.devs@gmail.com>
2022-01-16 15:15:14 -0800

    Add max character count error for CommentDetail
    Applied the error check for a user reaching the max character count when
    creating a comment in the textarea of the CommentDetail component

--- src/pages/FeedbackDetailPage/components/CommentsList/CommentForm.jsx
+++ src/pages/FeedbackDetailPage/components/CommentsList/CommentForm.jsx
@@ -1,20 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export const CommentForm = ({ styles }) => {
   const [commentInput, setCommentInput] = useState('');
   const [inputError, setErrorInputError] = useState(false);
+  const [charactersLeft, setCharactersLeft] = useState(250);
+  const [maxCharacterError, setMaxCharacterError] = useState(false);
 
-  const handleCommentInput = (e) => {
+  useEffect(() => {
+    if (charactersLeft <= 0) {
+      return setCharactersLeft(0);
+    }
+  }, [charactersLeft]);
+
+  const onCommentChange = (e) => {
     setErrorInputError(false);
     setCommentInput(e.target.value);
+    setCharactersLeft(250 - e.target.value.length);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!commentInput) {
-      return setErrorInputError(!inputError);
+      return setErrorInputError(true);
     }
 
+    if (commentInput.length >= 250) {
+      return setMaxCharacterError(true);
+    }
+
+    setMaxCharacterError(false);
     setErrorInputError(false);
     console.log('submitted');
   };
@@ -40,8 +54,11 @@ export const CommentForm = ({ styles }) => {
         </span>
         <textarea
           id="add-comment"
-          className={`${styles.textarea} ${inputError && styles.errorOutline}`}
-          onChange={handleCommentInput}
+          className={`${styles.textarea} ${
+            (inputError && styles.errorOutline) ||
+            (maxCharacterError && styles.errorOutline)
+          }`}
+          onChange={onCommentChange}
           name="add-comment"
           value={commentInput}
           rows="8"
@@ -49,7 +66,13 @@ export const CommentForm = ({ styles }) => {
         />
       </div>
       <div className={styles.buttonBox}>
-        <span className={styles.characterCountText}>250 characters left</span>
+        <span
+          className={`${styles.characterCountText} ${
+            commentInput.length >= 250 ? styles.charactersError : null
+          }`}
+        >
+          {`${charactersLeft} characters left`}
+        </span>
         <button className={styles.submitBtn} type="submit">
           post comment
         </button>
