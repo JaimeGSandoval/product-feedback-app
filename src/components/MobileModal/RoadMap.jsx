85aeb7295bfb798e5eac45f27f1a4331d24d2bc8
jaimegsandoval <jaime.devs@gmail.com>
2022-01-18 17:12:22 -0800

    Add UI task totals for roadmap in mobile modal
    Changed the roadmap total tasks data to represent data from local
    storage

--- src/components/MobileModal/RoadMap.jsx
+++ src/components/MobileModal/RoadMap.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './_mobileModal.module.scss';
 
-const RoadMapData = ({ data }) => {
+const RoadMapData = ({ roadmapData, taskTotal }) => {
   return (
     <div className={styles.roadMapRow}>
-      <div className={styles.roadMapCategoryBox}>
+      <div className={styles.roadMapStatusBox}>
         <span className={styles.colorCircle}></span>
-        <span className={styles.category}>{data.category}</span>
+        <span className={styles.status}>{roadmapData.status}</span>
       </div>
-      <span className={styles.taskAmount}>{data.tasks}</span>
+      <span className={styles.taskAmount}>{roadmapData.tasks}</span>
     </div>
   );
 };
