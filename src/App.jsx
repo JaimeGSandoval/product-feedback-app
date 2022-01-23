8c5b8e73367938fce22a53b8c7f3c949229cca7a
jaimegsandoval <jaime.devs@gmail.com>
2022-01-22 22:41:13 -0800

    Add route params for product requests
    Removed the mock data for product requests and replaced it with real
    requests. When clicked on, the user is taken to the specific product
    request.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import { SuggestionsPage } from './pages/SuggestionsPage';
 import { AddFeedbackPage } from './pages/AddFeedbackPage';
 import { FeedbackDetailPage } from './pages/FeedbackDetailPage';
-import { EditFeedback } from './pages/FeedbackDetailPage/components/EditFeedback';
 import { RoadmapPage } from './pages/RoadmapPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { keyboardDetectInit } from './utils/keyboardUserDetect';
@@ -24,8 +23,10 @@ function App() {
         <Routes>
           <Route path="/" element={<SuggestionsPage />} />
           <Route path="/add-feedback" element={<AddFeedbackPage />} />
-          <Route path="/feedback-detail" element={<FeedbackDetailPage />} />
-          <Route path="/edit-feedback" element={<EditFeedback />} />
+          <Route
+            path="/feedback-detail/:requestID"
+            element={<FeedbackDetailPage />}
+          />
           <Route path="/roadmap" element={<RoadmapPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
--- src/pages/FeedbackDetailPage/FeedbackDetailPage.jsx
+++ src/pages/FeedbackDetailPage/FeedbackDetailPage.jsx
@@ -1,11 +1,14 @@
+import { useParams } from 'react-router-dom';
 import { CommentsList } from './components/CommentsList';
 import styles from './components/CommentsList/_commentsList.module.scss';
 
 export const FeedbackDetailPage = () => {
+  const { requestID } = useParams();
+
   return (
     <>
       <div className={styles.container}>
-        <CommentsList />
+        <CommentsList requestID={requestID} />
       </div>
     </>
   );
