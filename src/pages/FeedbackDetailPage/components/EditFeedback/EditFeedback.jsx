9d7dc8cd52753d84903c5589b36e09164683d1a7
jaimegsandoval <jaime.devs@gmail.com>
2022-01-24 22:38:32 -0800

    Revert "Add upvote feature to feedback detail"
    This reverts commit f4398601a564ad021e6d18277eacc2124a6d15af.

--- src/pages/FeedbackDetailPage/components/EditFeedback/EditFeedback.jsx
+++ src/pages/FeedbackDetailPage/components/EditFeedback/EditFeedback.jsx
@@ -2,7 +2,7 @@ import { EditForm } from './components/EditForm';
 import { GoBackBtn } from '../../../../components/GoBackBtn';
 import styles from './_editFeedback.module.scss';
 
-export const EditFeedback = ({ request, setIsEditing, isEditing }) => {
+export const EditFeedback = ({ productRequest, setIsEditing, isEditing }) => {
   const goBack = () => setIsEditing(false);
 
   // const handleEditSubmit = () => setIsEditing(false);
@@ -10,7 +10,7 @@ export const EditFeedback = ({ request, setIsEditing, isEditing }) => {
   return (
     <>
       <GoBackBtn styles={styles} goBack={goBack} />
-      <EditForm request={request} />
+      <EditForm productRequest={productRequest} />
     </>
   );
 };
--- src/pages/FeedbackDetailPage/components/Suggestion/Suggestion.jsx
+++ src/pages/FeedbackDetailPage/components/Suggestion/Suggestion.jsx
@@ -1,43 +1,39 @@
-import { useContext } from 'react';
-import { DispatchContext } from '../../../../context/requests.context';
+import { useState } from 'react';
 import comment from '../../../../assets/icons/comments.svg';
 import arrowUp from '../../../../assets/icons/arrow-up.svg';
 import arrowUpWhite from '../../../../assets/icons/arrow-up-white.svg';
 import styles from './_suggestion.module.scss';
 
-export const Suggestion = ({ request }) => {
-  const dispatch = useContext(DispatchContext);
+export const Suggestion = ({ productRequest }) => {
+  const [active, setActive] = useState(false);
 
+  const handleUpVoteClick = () => setActive(!active);
   return (
     <>
       <div className={styles.container}>
         <div className={styles.innerContainer}>
           <div className={styles.textBox}>
-            <span className={styles.title}>{request.title}</span>
-            <p className={styles.description}>{request.description}</p>
-            <span className={styles.category}>{request.category}</span>
+            <span className={styles.title}>{productRequest.title}</span>
+            <p className={styles.description}>{productRequest.description}</p>
+            <span className={styles.category}>{productRequest.category}</span>
           </div>
 
           <div className={styles.likesBox}>
             <span
-              className={`${styles.likesTotal} ${
-                request.upvoted && styles.active
-              }`}
+              className={`${styles.likesTotal} ${active && styles.active}`}
               onClick={(e) => {
                 e.preventDefault();
-                dispatch({
-                  type: 'upvote',
-                  requestID: request.requestID,
-                });
+                console.log('yo');
+                handleUpVoteClick();
               }}
               tabIndex="0"
             >
               <img
                 className={styles.likeArrow}
-                src={request.upvoted ? arrowUpWhite : arrowUp}
+                src={active ? arrowUpWhite : arrowUp}
                 alt="arrow up"
               />
-              {request.upvotes}
+              {productRequest.upvotes}
             </span>
             <div className={styles.commentBox}>
               <img
@@ -46,7 +42,7 @@ export const Suggestion = ({ request }) => {
                 alt="comment bubble"
               />
               <span className={styles.commentsTotal}>
-                {request.comments ? request.comments.length : 0}
+                {productRequest.comments ? productRequest.comments.length : 0}
               </span>
             </div>
           </div>
