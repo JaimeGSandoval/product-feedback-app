05a504b8f0c5c8979dc56a64eec43da5d630dd06
jaimegsandoval <jaime.devs@gmail.com>
2022-02-12 15:52:07 -0800

    Add landscape styles for mobile
    Landscape implemented until a max-device-width of 926px. Changed it from
    min-width to min-device-width and max-device-width for the final
    landscape media query so that the landscape styles would not be applied
    to tablet or desktop devices

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,13 @@ import './index.scss';
 //   console.log(test);
 // }
 
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(document.querySelectorAll('*'), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
+
 function App() {
   return (
     <div className="App">
