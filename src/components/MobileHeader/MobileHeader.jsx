c6754019a8b0da8a90ade8fb770486ed1e4b07eb
JaimeGSandoval <jaime.devs@gmail.com>
2022-07-28 22:16:52 -0700

    Remove background gradient img for mobile header
    - Remove img from mobile header
    
    - Fixed broken styles for mobile header which was a result of gradient
    img being removed and replaced with a bg color. Fixed the styles for
    mobile portrait and mobile landscape

--- src/components/MobileHeader/MobileHeader.jsx
+++ src/components/MobileHeader/MobileHeader.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { MobileModal } from '../MobileModal';
-import mobileBgWebp from '../../assets/images/mobile/bg_header_sm.webp';
-import mobileBgPng from '../../assets/images/mobile/bg_header_sm.png';
-import desktopBgWebp from '../../assets/images/desktop/bg_header_lg.webp';
-import desktopBgPng from '../../assets/images/desktop/bg_header_lg.png';
-import hamburger from '../../assets/icons/hamburger.svg';
-import close from '../../assets/icons/close.svg';
-import styles from './_mobileHeader.module.scss';
+import { useState } from "react";
+import { MobileModal } from "../MobileModal";
+import mobileBgWebp from "../../assets/images/mobile/bg_header_sm.webp";
+import mobileBgPng from "../../assets/images/mobile/bg_header_sm.png";
+import desktopBgWebp from "../../assets/images/desktop/bg_header_lg.webp";
+import desktopBgPng from "../../assets/images/desktop/bg_header_lg.png";
+import hamburger from "../../assets/icons/hamburger.svg";
+import close from "../../assets/icons/close.svg";
+import styles from "./_mobileHeader.module.scss";
 
 export const MobileHeader = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -16,27 +16,14 @@ export const MobileHeader = () => {
   };
 
   const handleKeyPressModal = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       setIsModalOpen(!isModalOpen);
     }
   };
 
   return (
     <>
-      <header className={`${styles.header} ${isModalOpen ? styles.fixed : ''}`}>
-        <picture>
-          <source
-            srcSet={`${desktopBgWebp}, 1024w, ${desktopBgPng} 1024w`}
-            media="(min-width: 1024px)"
-            type="image/webp"
-          />
-          <source srcSet={mobileBgWebp} type="image/webp" />
-          <img
-            src={mobileBgPng}
-            className={styles.headerBgImg}
-            alt="header background"
-          />
-        </picture>
+      <header className={`${styles.header} ${isModalOpen ? styles.fixed : ""}`}>
         <div className={styles.contentBox}>
           <div className={styles.textContainer}>
             <span className={styles.headerText}>dynamic devs</span>
